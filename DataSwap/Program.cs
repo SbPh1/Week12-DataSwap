1e42a089a049f86da4f24bd980a18c1667cb32e9
H <sikkelh@gmail.com>
2021-11-17 10:20:15 +0200

    Data

---
+++ DataSwap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace DataSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string autumnFile = @"C:\Users\Monkey\samples\autumn.txt";
+            string winterFile = @"C:\Users\Monkey\samples\winter.txt";
+
+            string[] winterData = File.ReadAllLines(autumnFile);
+            string[] autumnData = File.ReadAllLines(winterFile);
+
+            File.WriteAllLines(autumnFile, autumnData);
+            File.WriteAllLines(winterFile, winterData);
+        }
+    }
+}
---
+++ RandomQuote/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace RandomQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\monkey\samples\quotes.txt";
+            string[] dataFromFile = File.ReadAllLines(filepath);
+
+            Console.WriteLine(dataFromFile[0]);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, dataFromFile.Length);
+            Console.WriteLine(dataFromFile[randomIndex]);
+        }
+        
+
+    }
+}
